f3347d7aee8dfd3010ce84848d232beea7f7eaa0
Nathan Clonts <nacclonts@gmail.com>
2018-06-13 16:21:13 -0600

    initial pups

--- javascript/avatar.js
+++ javascript/avatar.js
@@ -11,21 +11,94 @@ skinColors = {
     orange: 'hsl(38, 77%, 64%)',
 };
 
+//
+earTypes = {
+    droopy: (svg, attributes) => {
+        addPath(svg, 'M60 30 c 5 20, -40 30, -40 70 s -50 -80, 40 -70', attributes);
+        addPath(svg, 'M140 30 c -5 20, +40 30, +40 70 s +50 -80, -40 -70', attributes);
+    },
+    pointy: (svg, attributes) => {
+        let att = attributes.concat(['stroke', 'transparent'])
+        // addPath(svg, 'M22 85 c -5 -50, 10 -80, 15 -85 s 00 00, 30 25 S 30 40, 22 95', att);
+        addPath(svg, 'M70 45 c -5 -10, -10 -15, -25 -35 s -10 15, -20 55 S 30 50, 70 45', att);
+        // addPath(svg, 'M178 90 c +5 -50, -10 -80, -15 -85 s 00 00, -30 20 S 170 40, 178 90', att);
+        // addPath(svg, 'M178 80 c +5 -50, -10 -80, -15 -77 s 00 00, -30 20 S 170 40, 178 80', attributes)
+    }
+}
+
 window.onload = function() {
     drawAllTheCats()
+    //
+    // let svg = document.getElementById('rootSvg');
+    // svg.addEventListener('mouseover', (ev) => {
+    //     console.log(`X: ${ev.clientX}, Y: ${ev.clientY}`)
+    // });
+    // drawDog(eyeColors.blue, skinColors.black, earTypes.pointy, svg)
 }
+
+
 
 function drawAllTheCats() {
     let svg = document.getElementById('rootSvg');
     for (let eye of Object.keys(eyeColors)) {
         for (let skin of Object.keys(skinColors)) {
-            let s2 = svg.cloneNode();
+            let s2 = svg.cloneNode(true);
             drawCat(eyeColors[eye], skinColors[skin], s2);
             document.body.appendChild(s2)
         }
     }
-    svg.remove();
+    svg.setAttribute('visibility', 'collapse');
 }
+
+function drawDog(eyeColor, skinColor, addEar, svgElement) {
+    let s = svgElement;
+    let skin = [
+        ['fill', skinColor],
+        ['stroke', skinColor]
+    ];
+
+    // bottom of head
+    addPath(s, "M20 100 C 40 200, 160 200, 180 100", skin);
+    // top of head
+    addPath(s, "M20 100 C 20 20, 180 20, 180 100", skin);
+
+    // eyes
+    let eye = [
+        ['fill', eyeColor],
+        ['stroke-width', '1']
+    ];
+    let pupil = [
+        ['class', 'pupil'], ['fill', '#222'], ['stroke', 'none']
+    ];
+    // left eye
+    addPath(s, "M40 85 c 00 -20, 40 -30, 40 0", eye);
+    addPath(s, "M40 85 c 00 +20, 40 +20, 40 0", eye);
+    addCircle(s, [60, 80], 10, pupil);
+    addCircle(s, [60, 80], 2, [['class', 'pupil-reflection'], ['fill', 'white']]);
+
+    // right eye
+    addPath(s, "M120 85 c 00 -30, 40 -20, 40 0", eye);
+    addPath(s, "M120 85 c 00 +20, 40 +20, 40 0", eye);
+    addCircle(s, [140, 80], 10, pupil);
+    addCircle(s, [140, 80], 2, [['class', 'pupil-reflection'], ['fill', 'white']]);
+
+
+    // mouth
+    addPath(s, 'M65 140 c 00 10, 30 15, 35 00');
+    addPath(s, 'M100 140 c 00 10, 30 15, 35 00');
+
+    // ears
+    let ear = [
+        ['fill', skinColor],
+        ['stroke-width', '1']
+    ];
+    addEar(s, ear);
+
+
+    // nose
+    addPolygon(s, [[85, 115], [115, 115], [100, 130]], [['fill', 'hsl(0, 36%, 51%)']]);
+};
+
 
 function drawCat(eyeColor, skinColor, svgElement) {
     let s = svgElement;
@@ -45,21 +118,24 @@ function drawCat(eyeColor, skinColor, svgElement) {
         ['stroke-width', '1']
     ];
     let pupil = [
-        ['class', 'pupil'], ['fill', '#222'], ['stroke', 'none']
+        ['class', 'pupil-cat'], ['fill', '#222'], ['stroke', 'none']
+    ];
+    let pupilReflection = [
+        ['class', 'pupil-reflection-cat'], ['fill', 'white']
     ];
     // left eye
     addPath(s, "M40 80 c 00 -30, 40 -30, 40 0", eye);
     addPath(s, "M40 80 c 00 +30, 40 +30, 40 0", eye);
     addPath(s, 'M60, 65 c -10 10, -10 20, 0 30', pupil);
     addPath(s, 'M60, 65 c +10 10, +10 20, 0 30', pupil);
-    addCircle(s, [60, 75], 2, [['class', 'pupil-reflection'], ['fill', 'white']]);
+    addCircle(s, [60, 75], 2, pupilReflection);
 
     // right eye
     addPath(s, "M120 80 c 00 -30, 40 -30, 40 0", eye);
     addPath(s, "M120 80 c 00 +30, 40 +30, 40 0", eye);
     addPath(s, 'M140, 65 c -10 10, -10 20, 0 30', pupil);
     addPath(s, 'M140, 65 c +10 10, +10 20, 0 30', pupil);
-    addCircle(s, [140, 75], 2, [['class', 'pupil-reflection'], ['fill', 'white']]);
+    addCircle(s, [140, 75], 2, pupilReflection);
 
 
     // mouth
@@ -78,10 +154,10 @@ function drawCat(eyeColor, skinColor, svgElement) {
         top: [200 - ecLeft.top[0], ecLeft.top[1]]
     }
     addPolygon(s, [ecLeft.outside, ecLeft.inside, ecLeft.top],
-        [['fill', skinColor]]
+        [['fill', skinColor], ['stroke-linejoin', 'round']]
     );
     addPolygon(s, [ecRight.outside, ecRight.inside, ecRight.top],
-        [['fill', skinColor]]
+        [['fill', skinColor], ['stroke-linejoin', 'round']]
     );
     // pink inner ear
     addPolygon(s, [
