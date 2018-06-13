0b24aefb6cca1bf7da131c33248df4519938f234
Nathan Clonts <nacclonts@gmail.com>
2018-06-13 14:24:53 -0600

    draw all the cats!

--- javascript/cat.js
+++ javascript/cat.js
@@ -2,7 +2,7 @@ eyeColors = {
     green: 'hsl(82, 83%, 52%)',
     blue: 'hsl(194, 79%, 54%)',
     grey: 'hsl(210, 8%, 95%)',
-    orange: 'hsl(41, 100%, 72%)'
+    yellow: 'hsl(61, 100%, 70%)'
 };
 
 skinColors = {
@@ -12,19 +12,28 @@ skinColors = {
 };
 
 window.onload = function() {
-    drawCat(eyeColors.blue, skinColors.black, );
+    let svg = document.getElementById('rootSvg');
+    for (let eye of Object.keys(eyeColors)) {
+        for (let skin of Object.keys(skinColors)) {
+            let s2 = svg.cloneNode();
+            drawCat(eyeColors[eye], skinColors[skin], s2);
+            document.body.appendChild(s2)
+        }
+    }
+    svg.remove();
 }
 
-function drawCat(eyeColor, skinColor) {
+function drawCat(eyeColor, skinColor, svgElement) {
+    let s = svgElement;
     let skin = [
         ['fill', skinColor],
         ['stroke', skinColor]
     ];
 
     // bottom of head
-    addPath("M20 100 C 20 200, 180 200, 180 100", skin);
+    addPath(s, "M20 100 C 20 200, 180 200, 180 100", skin);
     // top of head
-    addPath("M20 100 C 20 000, 180 000, 180 100", skin);
+    addPath(s, "M20 100 C 20 000, 180 000, 180 100", skin);
 
     // eyes
     let eye = [
@@ -35,23 +44,23 @@ function drawCat(eyeColor, skinColor) {
         ['class', 'pupil'], ['fill', '#222'], ['stroke', 'none']
     ];
     // left eye
-    addPath("M40 80 c 00 -30, 40 -30, 40 0", eye);
-    addPath("M40 80 c 00 +30, 40 +30, 40 0", eye);
-    addPath('M60, 65 c -10 10, -10 20, 0 30', pupil);
-    addPath('M60, 65 c +10 10, +10 20, 0 30', pupil);
-    addCircle([60, 75], 2, [['class', 'pupil-reflection'], ['fill', 'white']]);
+    addPath(s, "M40 80 c 00 -30, 40 -30, 40 0", eye);
+    addPath(s, "M40 80 c 00 +30, 40 +30, 40 0", eye);
+    addPath(s, 'M60, 65 c -10 10, -10 20, 0 30', pupil);
+    addPath(s, 'M60, 65 c +10 10, +10 20, 0 30', pupil);
+    addCircle(s, [60, 75], 2, [['class', 'pupil-reflection'], ['fill', 'white']]);
 
     // right eye
-    addPath("M120 80 c 00 -30, 40 -30, 40 0", eye);
-    addPath("M120 80 c 00 +30, 40 +30, 40 0", eye);
-    addPath('M140, 65 c -10 10, -10 20, 0 30', pupil);
-    addPath('M140, 65 c +10 10, +10 20, 0 30', pupil);
-    addCircle([140, 75], 2, [['class', 'pupil-reflection'], ['fill', 'white']]);
+    addPath(s, "M120 80 c 00 -30, 40 -30, 40 0", eye);
+    addPath(s, "M120 80 c 00 +30, 40 +30, 40 0", eye);
+    addPath(s, 'M140, 65 c -10 10, -10 20, 0 30', pupil);
+    addPath(s, 'M140, 65 c +10 10, +10 20, 0 30', pupil);
+    addCircle(s, [140, 75], 2, [['class', 'pupil-reflection'], ['fill', 'white']]);
 
 
     // mouth
-    addPath('M65 140 c 00 10, 30 30, 35 00');
-    addPath('M100 140 c 00 10, 10 30, 35 00');
+    addPath(s, 'M65 140 c 00 10, 30 30, 35 00');
+    addPath(s, 'M100 140 c 00 10, 10 30, 35 00');
 
     // ears
     let ecLeft = { // ear coordinates
@@ -64,21 +73,21 @@ function drawCat(eyeColor, skinColor) {
         inside: [200 - ecLeft.inside[0], ecLeft.inside[1]],
         top: [200 - ecLeft.top[0], ecLeft.top[1]]
     }
-    addPolygon([ecLeft.outside, ecLeft.inside, ecLeft.top],
+    addPolygon(s, [ecLeft.outside, ecLeft.inside, ecLeft.top],
         [['fill', skinColor]]
     );
-    addPolygon([ecRight.outside, ecRight.inside, ecRight.top],
+    addPolygon(s, [ecRight.outside, ecRight.inside, ecRight.top],
         [['fill', skinColor]]
     );
     // pink inner ear
-    addPolygon([
+    addPolygon(s, [
             vAdd(ecLeft.outside, [7,-30]),
             vAdd(ecLeft.inside, [-15, -5]),
             vAdd(ecLeft.top, [2, 5])
         ],
         [['fill', 'hsl(0, 36%, 60%)']]
     );
-    addPolygon([
+    addPolygon(s, [
             vAdd(ecRight.outside, [-7,-30]),
             vAdd(ecRight.inside, [+15, -5]),
             vAdd(ecRight.top, [-2, 5])
@@ -86,32 +95,35 @@ function drawCat(eyeColor, skinColor) {
         [['fill', 'hsl(0, 36%, 60%)']]
     );
     // ear hairs
+    let earHair = [['stroke', skinColor]];
     for (let i=0; i < 10; i++) {
-        addLine([ecLeft.outside[0] + 7 + i*3, ecLeft.outside[1] - 25 - i*2],
-                [ecLeft.outside[0] + 0 + i*3, ecLeft.outside[1] - 40 - i*2.5]);
-        addLine([ecRight.outside[0] - 7 - i*3, ecRight.outside[1] - 25 - i*2],
-                [ecRight.outside[0] + 0 - i*3, ecRight.outside[1] - 40 - i*2.5]);
+        addLine(s, [ecLeft.outside[0] + 7 + i*3, ecLeft.outside[1] - 25 - i*2],
+                [ecLeft.outside[0] + 0 + i*3, ecLeft.outside[1] - 40 - i*2.5],
+                earHair);
+        addLine(s, [ecRight.outside[0] - 7 - i*3, ecRight.outside[1] - 25 - i*2],
+                [ecRight.outside[0] + 0 - i*3, ecRight.outside[1] - 40 - i*2.5],
+                earHair);
     }
 
     // nose
-    addPolygon([[90, 115], [110, 115], [100, 130]], [['fill', 'hsl(0, 36%, 51%)']]);
-    addLine([100, 129], [100, 140], [['stroke', '#666'], ['stroke-width', '2']]);
+    addPolygon(s, [[90, 115], [110, 115], [100, 130]], [['fill', 'hsl(0, 36%, 51%)']]);
+    addLine(s, [100, 129], [100, 140], [['stroke', '#666'], ['stroke-width', '2']]);
 
     // whiskers
-    drawWhiskers([58, 120], false);
-    drawWhiskers([142, 120], true);
+    drawWhiskers(s, [58, 120], false);
+    drawWhiskers(s, [142, 120], true);
 
     // draw grid
     // for (let i=0; i < 10; i++) {
     //     // vertical line
-    //     addLine([i * 20, 0], [i * 20, 200]);
+    //     addLine(s, [i * 20, 0], [i * 20, 200]);
     //     // horizontal line
-    //     addLine([0, i * 20], [200, i * 20]);
+    //     addLine(s, [0, i * 20], [200, i * 20]);
     // }
 };
 
 
-function drawWhiskers(point, inverse=false) {
+function drawWhiskers(s, point, inverse=false) {
     let attributes = [
         ['stroke-width', '2'],
         ['stroke', inverse ? 'url(#whisker-gradient-reverse)' : 'url(#whisker-gradient)']
@@ -124,9 +136,9 @@ function drawWhiskers(point, inverse=false) {
     let c3 = [[-25, 10], [-50,   5], [-54,  10]].map(applySign);
 
 
-    addPath(`M${point[0]} ${point[1]}   c ${pointsToString(c1)}`, attributes);
-    addPath(`M${point[0]-3*sign} ${point[1]+5} c ${pointsToString(c2)}`, attributes);
-    addPath(`M${point[0]+1*sign} ${point[1]+10} c ${pointsToString(c3)}`, attributes);
+    addPath(s, `M${point[0]} ${point[1]}   c ${pointsToString(c1)}`, attributes);
+    addPath(s, `M${point[0]-3*sign} ${point[1]+5} c ${pointsToString(c2)}`, attributes);
+    addPath(s, `M${point[0]+1*sign} ${point[1]+10} c ${pointsToString(c3)}`, attributes);
 }
 
 function pointsToString(points) {
@@ -138,25 +150,25 @@ function pointsToString(points) {
     }, '').substr(2);
 }
 
-function addPolygon(points, attributes=[]) {
+function addPolygon(s, points, attributes=[]) {
     let defaultAttributes = [
         ['points', pointsToString(points)]
     ];
 
-    addSvg('polygon', defaultAttributes.concat(attributes));
+    addSvg(s, 'polygon', defaultAttributes.concat(attributes));
 }
 
-function addPath(points, attributes=[]) {
+function addPath(s, points, attributes=[]) {
     let defaultAttributes = [
         ['d', points],
         ['stroke', '#666'],
         ['stroke-width', '3'],
         ['fill', 'transparent']
     ];
-    addSvg('path', defaultAttributes.concat(attributes));
+    addSvg(s, 'path', defaultAttributes.concat(attributes));
 }
 
-function addLine(p1, p2, attributes=[]) {
+function addLine(s, p1, p2, attributes=[]) {
     let defaultAttributes = [
         ['x1', p1[0]],
         ['y1', p1[1]],
@@ -165,25 +177,25 @@ function addLine(p1, p2, attributes=[]) {
         ['stroke', 'rgba(176, 176, 176, 0.4)'],
         ['stroke-width', '1']
     ];
-    addSvg('line', defaultAttributes.concat(attributes));
+    addSvg(s, 'line', defaultAttributes.concat(attributes));
 }
 
-function addCircle(center, radius, attributes=[]) {
+function addCircle(s, center, radius, attributes=[]) {
     let defaultAttributes = [
         ['cx', center[0]],
         ['cy', center[1]],
         ['r', radius],
         ['fill', '#444']
     ];
-    addSvg('circle', defaultAttributes.concat(attributes));
+    addSvg(s, 'circle', defaultAttributes.concat(attributes));
 }
 
 function addGradient() {
 
 }
 
-function addSvg(type, attributes) {
-    let root = document.getElementById('rootSvg');
+function addSvg(s, type, attributes) {
+    let root = s;
     let child = document.createElementNS(root.namespaceURI, type);
     for (let attr of attributes) {
         child.setAttribute(attr[0], attr[1]);
